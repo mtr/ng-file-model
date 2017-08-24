cfdc7357dfcab6f0a642feebc2df92b8d4a8e200
Martin Thorsen Ranang <mtr@ranang.org>
2017-08-24 08:54:08 +0200

    Integrated improvements from https://github.com/TheoSqua/ng-file-model.

--- ng-file-model.js
+++ ng-file-model.js
@@ -1,33 +1,45 @@
 (function () {
     'use strict';
-    angular.module('ng-file-model', [])
-    .directive("ngFileModel", [function () {
+    angular.module('ng-file-model', []).directive("ngFileModel", [function () {
         return {
             scope: {
                 ngFileModel: "="
             },
             link: function (scope, element, attributes) {
                 element.bind("change", function (changeEvent) {
-                    var reader = new FileReader();
-                    reader.onload = function (loadEvent) {
-                        scope.$apply(function () {
-                            scope.ngFileModel = {
-                                lastModified: changeEvent.target.files[0].lastModified,
-                                lastModifiedDate: changeEvent.target.files[0].lastModifiedDate,
-                                name: changeEvent.target.files[0].name,
-                                size: changeEvent.target.files[0].size,
-                                type: changeEvent.target.files[0].type,
-                                data: loadEvent.target.result
-                            };
-                        });
+                    for (var i = 0; i < changeEvent.target.files.length; i++) {
+                        setupReader(changeEvent.target.files[i]);
                     }
-                    reader.readAsDataURL(changeEvent.target.files[0]);
+
+                    function setupReader(file) {
+                        var fileObj = {},
+                            reader = new FileReader();
+
+                        reader.onload = function (e) {
+                            scope.$apply(function () {
+                                var eSplit = e.target.result.split(',');
+                                fileObj = {
+                                    lastModified: file.lastModified,
+                                    lastModifiedDate: file.lastModifiedDate,
+                                    name: file.name,
+                                    size: file.size,
+                                    type: file.type,
+                                    data: eSplit[1]
+                                };
+                                scope.ngFileModel.push(fileObj);
+                            });
+                            element[0].value = '';
+                        };
+                        //this sets the value of e.target.results.
+                        reader.readAsDataURL(file);
+                    }
                 });
             }
-        }
+        };
     }]);
-    if( typeof exports !== 'undefined' ) {
-      exports['default'] = angular.module('ng-file-model');
-      module.exports = exports['default'];
+
+    if (typeof exports !== 'undefined') {
+        exports['default'] = angular.module('ng-file-model');
+        module.exports = exports['default'];
     }
 })();
